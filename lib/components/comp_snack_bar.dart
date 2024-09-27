653c1f4197afaa4c80ad6bc48e8169d5128bf86e
freebird920 <freebird_han@naver.com>
2024-09-27 21:44:51 +0900

    Enhance snackbar functionality by adding hideCurrentSnackBar logic and adjusting default duration

--- lib/components/comp_snack_bar.dart
+++ lib/components/comp_snack_bar.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:inghub_pomo/app/layout.dart';
 
 void openCompSnackBar({required String message, Duration? duration}) {
+  scaffoldMessengerKey.currentState?.hideCurrentSnackBar();
   scaffoldMessengerKey.currentState?.showSnackBar(
     SnackBar(
       content: Text(message),
+      duration: duration ?? const Duration(milliseconds: 1000),
       action: SnackBarAction(
         label: "닫기",
-        onPressed: () {},
+        onPressed: () {
+          scaffoldMessengerKey.currentState?.hideCurrentSnackBar();
+        },
       ),
-      duration: duration ?? const Duration(milliseconds: 500),
     ),
   );
 }
